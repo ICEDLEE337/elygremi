ea8b41018a575f9fb35b2f7e1ff7b416a5df94cf
icedlee337 <lee.e.norris@gmail.com>
2023-06-06 23:24:18 -0400

    chore: use dns host for ui-embedded

--- apps/ui/account-embedded/src/environments/environment.prod.ts
+++ apps/ui/account-embedded/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ import { Config } from "@oninet/ui/common";
 
 export const environment: Config = {
   production: true,
-  api: '',
+  api: 'https://account.onivoro.net',
   domain: '.onivoro.net',
   uiUrlAccount: 'https://account.onivoro.net',
 };
