dceda3f54447dfe645694dbcc19dc632e6c098bd
icedlee337 <lee.e.norris@gmail.com>
2023-06-15 18:50:52 -0400

    chore: still not working

--- libs/api/feature-account/src/lib/controllers/akeneo-product.controller.ts
+++ libs/api/feature-account/src/lib/controllers/akeneo-product.controller.ts
@@ -12,6 +12,7 @@ import {
 } from '@nestjs/common';
 import { ApiParam, ApiResponse } from '@nestjs/swagger';
 import { ChannelApi } from '@oninet/generated/akeneo';
+import { Channel } from 'diagnostics_channel';
 
 ////@UseGuards(AuthGuard)
 @Controller('akeneo-product')
@@ -29,6 +30,7 @@ export class AkeneoProductController {
         basePath: configureEnv().AKANEO_HOST,
         Headers: {
           Accept: 'application/json',
+          "Content-Type": 'application/json',
           Authorization
         }
       } as any;
@@ -46,9 +48,7 @@ export class AkeneoProductController {
 
 async function getToken() {
   try {
-    const str = `${configureEnv().AKANEO_CLIENT_ID}:${configureEnv().AKANEO_CLIENT_SECRET}`;
-    console.log(str);
-    const base64 = Buffer.from(str).toString('base64');
+    const str = (`${('1_3m3vja8ogl8g8w4s4cksgwgkgg4cwccs0kwow0o4osgwgkowwc')}:${encode('4k8t5gch6yyo8c0c8g4w8s0wcg0g4wsggs0w4o0gs40k0gw8g8')}`);
     return (await axios.post(`${configureEnv().AKANEO_HOST}/api/oauth/v1/token`,
       {
         "grant_type": "password",
@@ -57,12 +57,17 @@ async function getToken() {
       },
       {
         headers: {
+          Accept: 'application/json',
           "Content-Type": "application/json",
-          "Authorization": `Basic ${base64}`,
+          "Authorization": `Basic ${str}`,
         }
       })).data;
 
   } catch (error) {
     console.log('failed to get token => ', error);
   }
+}
+
+function encode (str: string) {
+  return Buffer.from(str).toString('base64');
 }
